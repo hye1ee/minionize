e389bdefcc4deb59eb0ac9a255f8a8d918d9f218
hye1ee <hyewon0809@kaist.ac.kr>
2024-03-07 18:10:03 -0500

    [chore] Solve error

--- src/pages/Mirror.tsx
+++ src/pages/Mirror.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { PageWrapper } from "../components/PageWrapper";
 import { useState } from "react";
 import MaskGen from "./MaskGen";
