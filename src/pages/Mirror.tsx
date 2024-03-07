f94d5e69ee160f5671a4cf937d27f59a165c0617
hye1ee <hyewon0809@kaist.ac.kr>
2024-03-07 18:09:14 -0500

    [styke] Change button style

--- src/pages/Mirror.tsx
+++ src/pages/Mirror.tsx
@@ -3,26 +3,27 @@ import { PageWrapper } from "../components/PageWrapper";
 import { useState } from "react";
 import MaskGen from "./MaskGen";
 import MaskTry from "./MaskTry";
+import styled from "styled-components";
 
 const Mirror = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const [maskData, setMaskData] = useState<string>("");
   const [maskGen, setMaskGen] = useState<boolean>(true);
 
   return (
     <PageWrapper>
-      <button
+      {/* <button
         onClick={() => navigate("/")}
         style={{ position: "absolute", right: 0, zIndex: 100 }}
       >
         Home
-      </button>
-      <button
-        onClick={() => setMaskGen((val) => !val)}
+      </button> */}
+      {/* <button
         style={{ position: "absolute", right: 0, top: "30px", zIndex: 100 }}
-      >
-        Mode Change
-      </button>
+      ></button> */}
+      <ButtonTitle onClick={() => setMaskGen((val) => !val)}>
+        {maskGen ? "Enter" : "Leave"}
+      </ButtonTitle>
       {maskGen ? (
         <MaskGen setMaskData={setMaskData} />
       ) : (
@@ -32,3 +33,24 @@ const Mirror = () => {
   );
 };
 export default Mirror;
+
+const ButtonTitle = styled.div`
+  font-family: "Rubik Bubbles", system-ui;
+  font-weight: 400;
+  font-style: normal;
+
+  font-size: 80px;
+  color: rgba(255, 255, 255, 0.2);
+  /* -webkit-text-stroke: 1px black; */
+
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  transition: all 1s;
+  z-index: 100;
+
+  cursor: pointer;
+  &:hover {
+    color: rgba(255, 255, 255, 0.7);
+  }
+`;
