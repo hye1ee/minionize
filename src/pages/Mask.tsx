73bbbb628d3dccbcbaed313930a1c085cd034d04
hye1ee <hyewon0809@kaist.ac.kr>
2024-03-06 20:12:19 -0500

    [feat] Implement ar stage

--- src/pages/Mask.tsx
+++ src/pages/Mask.tsx
@@ -1,6 +1,95 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as THREE from "three";
+import { MindARThree } from "mind-ar/dist/mindar-face-three.prod.js";
+import { useEffect, useState } from "react";
 import { PageWrapper } from "../components/PageWrapper";
+import styled from "styled-components";
 
-const Mask = () => {
-  return <PageWrapper></PageWrapper>;
+interface MaskGenProps {
+  setMaskData: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const MaskGen = (props: MaskGenProps) => {
+  return (
+    <PageWrapper>
+      This is generation page
+      <MaskGenCanvas id="mask-canvas" />
+    </PageWrapper>
+  );
 };
-export default Mask;
+const MaskGenCanvas = styled.canvas`
+  width: "400px";
+  height: "400px";
+  border: "1px solid black";
+`;
+
+interface MaskTryProps {
+  maskData: string;
+}
+export const MaskTry = (props: MaskTryProps) => {
+  const [faceMesh, setFaceMesh] = useState<any>(null);
+  const [mindar, setMindar] = useState<any>(null);
+
+  const start = async () => {
+    const { renderer, scene, camera } = mindar;
+    await mindar.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+  };
+
+  useEffect(() => {
+    // generate ar instance
+    const newMindar = new MindARThree({
+      container: document.querySelector("#mask-render"),
+    });
+    setMindar(newMindar);
+  }, []);
+
+  useEffect(() => {
+    // generate face instance
+    if (!mindar) return;
+
+    const { scene } = mindar;
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+    scene.add(light);
+
+    const newFaceMesh = mindar.addFaceMesh();
+    setFaceMesh(newFaceMesh);
+  }, [mindar]);
+
+  useEffect(() => {
+    // load face texture
+    if (!faceMesh) return;
+    const { scene } = mindar;
+    const texture = new THREE.TextureLoader().load(props.maskData);
+    faceMesh.material.map = texture;
+    faceMesh.material.transparent = true;
+    faceMesh.material.needsUpdate = true;
+    scene.add(faceMesh);
+
+    start();
+
+    return () => {
+      if (mindar) {
+        console.log("stop");
+        mindar.renderer.setAnimationLoop(null);
+        mindar.stop();
+      }
+    };
+  }, [faceMesh]);
+
+  return (
+    <PageWrapper>
+      This is try page
+      <MaskRenderContainer id="mask-render" />
+    </PageWrapper>
+  );
+};
+
+const MaskRenderContainer = styled.div`
+  width: 500px;
+  height: auto;
+
+  position: relative;
+`;
--- src/pages/Mirror.tsx
+++ src/pages/Mirror.tsx
@@ -1,13 +1,28 @@
 import { useNavigate } from "react-router-dom";
 import { PageWrapper } from "../components/PageWrapper";
+import { MaskGen, MaskTry } from "./Mask";
+import { useState } from "react";
+import MaskImg from "../assets/minions.png";
 
 const Mirror = () => {
   const navigate = useNavigate();
+  const [maskGen, setMaskGen] = useState<boolean>(true);
 
   return (
     <PageWrapper>
-      this is mirror room
-      <button onClick={() => navigate("/")} />
+      <button
+        onClick={() => navigate("/")}
+        style={{ position: "absolute", right: 0 }}
+      >
+        Home
+      </button>
+      <button
+        onClick={() => setMaskGen((val) => !val)}
+        style={{ position: "absolute", right: 0, top: "30px" }}
+      >
+        Mode Change
+      </button>
+      {maskGen ? <MaskGen /> : <MaskTry maskData={MaskImg} />}
     </PageWrapper>
   );
 };
